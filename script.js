f8c875f173291590baca04f2b6b64555050637a1
Admin <duckjopa200@gmail.com>
2026-02-06 22:57:43 +0200

    cookie-clicker working

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/*TODO-list:
+1) CLick counter removal, button that start counting from 0 again;
+3) Working nav-buttons; 
+2) Comment sectioń, publib place where webpage visitors can leave a message what should i add next;
+*) Dark mode
+
+*/
+
+
+
+
+
 let count = 0 
 
 const button = document.querySelector("#cookieBtn")
